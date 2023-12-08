48faf6ce913d840cacfb6a96447e285fb1a9c505
arefin6 <arefinhossain3@gmail.com>
2023-12-08 19:20:54 +0600

    counter Slice Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 import { RootState } from './redux/store'
+import { decrement, increment } from './redux/features/counter/counterSlice'
 function App() {
   const count = useSelector((state: RootState) => state.count.count)
+  const dispatch = useDispatch()
   return (
     <div>
     <div>
       <button
         aria-label="Increment value"
-        // onClick={() => dispatch(increment())}
+        onClick={() => dispatch(increment())}
       >
         Increment
       </button>
       <span>{count}</span>
       <button
         aria-label="Decrement value"
-        // onClick={() => dispatch(decrement())}
+        onClick={() => dispatch(decrement())}
       >
         Decrement
       </button>
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -17,5 +17,6 @@ const counterSlice= createSlice({
     }
 })
 
+export const {increment,decrement}=counterSlice.actions
 
 export default counterSlice.reducer
