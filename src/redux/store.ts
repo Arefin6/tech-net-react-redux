20e6275df05e8f696e34c9c3014d5c05bebd1d88
arefin6 <arefinhossain3@gmail.com>
2024-01-15 19:30:51 +0600

    api layer created

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import CartSlice from './features/Cart/CartSlice';
 import ProductSlice from './features/Product/ProductSlice';
+import { api } from './api/apiSlice';
 
 const store = configureStore({
    reducer:{
       cart:CartSlice,
-      product:ProductSlice
-   }
+      product:ProductSlice,
+      [api.reducerPath]: api.reducer,
+   },
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
